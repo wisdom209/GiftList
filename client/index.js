dc57e06a0e6a971fa7637b1809d0aebe57668083
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-12-09 05:42:55 +0100

    Assessment Completed

--- client/index.js
+++ client/index.js
@@ -1,17 +1,23 @@
 const axios = require('axios');
-const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+	// TODO: how do we prove to the server we're on the nice list? 
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+	const name = "Norman Block"
+	const merkleTree = new MerkleTree(niceList)
+	const index = niceList.findIndex(n => n === name);
+	const proof = merkleTree.getProof(index);
 
-  console.log({ gift });
+	const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+		name,
+		proof
+	});
+
+	console.log({ gift });
 }
 
 main();
